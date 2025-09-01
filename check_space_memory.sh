102f97341b598954cbdc1031ac5409d817e0172f
Alchemyfinance <samong.ttttt@gmail.com>
2025-09-01 21:08:09 +0800

    Create check_space_memory.sh

---
+++ check_space_memory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check disk space
+echo "Disk Space Usage:"
+df -h  # '-h' gives human-readable format
+
+# Check memory usage
+echo "Memory Usage:"
+free -h  # '-h' for human-readable format
